36802e990779730d3db13d27b67c1aadbea92d95
omarcotamedina28@gmail.com <omarcotamedina18@gmail.com>
2024-12-10 14:42:13 -0700

    Agregar archivos de proyecto.

---
+++ RAppsWeb_OCME/RAppsWeb_OCME/Models/Pmodelo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RAppsWeb_OCME.Models
+{
+    public class Pmodelo
+    {
+        public double MontoCuenta { get; set; }
+        public double PorcentajePropina { get; set; }
+        public int NumeroPersonas { get; set; }
+
+        public double TotalPropina => (MontoCuenta * PorcentajePropina) / 100;
+
+        public double TotalPagar => MontoCuenta + TotalPropina;
+
+        public double MontoPorPersona => NumeroPersonas > 0 ? TotalPagar / NumeroPersonas : 0;
+
+        public string Carita => PorcentajePropina >= 10 ? "https://i.ibb.co/VT3n5NQ/feli.png" : "https://i.ibb.co/bFRrN0X/tiste.png";
+    }
+}
+
+
+
---
+++ RAppsWeb_OCME/RAppsWeb_OCME/ViewModel/PModeloVista.cs
@@ -0,0 +1,78 @@
+﻿using RAppsWeb_OCME.Models;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+
+namespace RAppsWeb_OCME.ViewModel
+{
+    public partial class PModeloVista : INotifyPropertyChanged
+    {
+        private double _montoCuenta;
+        private double _porcentajePropina;
+        private int _numeroPersonas;
+
+        public Pmodelo Modelo { get; set; }
+
+        public double MontoCuenta
+        {
+            get => _montoCuenta;
+            set
+            {
+                _montoCuenta = value;
+                OnPropertyChanged();
+                ActualizarPropina();
+            }
+        }
+
+        public double PorcentajePropina
+        {
+            get => _porcentajePropina;
+            set
+            {
+                _porcentajePropina = value;
+                OnPropertyChanged();
+                ActualizarPropina();
+            }
+        }
+
+        public int NumeroPersonas
+        {
+            get => _numeroPersonas;
+            set
+            {
+                _numeroPersonas = value;
+                OnPropertyChanged();
+                ActualizarPropina();
+            }
+        }
+
+        public double TotalPropina => Modelo.TotalPropina;
+        public double TotalPagar => Modelo.TotalPagar;
+        public double MontoPorPersona => Modelo.MontoPorPersona;
+        public string Carita => Modelo.Carita;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public PModeloVista()
+        {
+            Modelo = new Pmodelo();
+        }
+
+        private void ActualizarPropina()
+        {
+            Modelo.MontoCuenta = MontoCuenta;
+            Modelo.PorcentajePropina = PorcentajePropina;
+            Modelo.NumeroPersonas = NumeroPersonas;
+
+            OnPropertyChanged(nameof(TotalPropina));
+            OnPropertyChanged(nameof(TotalPagar));
+            OnPropertyChanged(nameof(MontoPorPersona));
+            OnPropertyChanged(nameof(Carita));
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
